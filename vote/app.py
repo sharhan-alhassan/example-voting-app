53dd822996b178a760601d56014eb99fbde3e5e5
sharhan-alhassan <sharhanalhassan@gmail.com>
2025-03-05 14:38:39 +0000

    feat: redeploy

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs and Sheep")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
